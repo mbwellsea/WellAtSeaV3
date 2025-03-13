6b64f51d69a2147eb65f2583b0ebbbc3778b502a
Marnel Barnatia <mb@wellatsea.com>
2025-03-13 22:17:09 +0800

    Add project files.

---
+++ WellAtSeaV3/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace WellAtSeaV3
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
